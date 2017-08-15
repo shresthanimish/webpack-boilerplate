72898089a6eec196fe92ca4d1a7f97adeed46b45
nimishshrestha <shrestha.nimish@gmail.com>
2017-08-15 13:12:09 +1000

    Added support for css importing and bundling.

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,14 @@ var path = require('path'); //core node js package to "resolve" path. Supports o
  * path: used by webpack prod to locate the output directory
  * publicPath: used by webpack-dev-server to locate the output directory
  *
+ * module:
+ *      rules:  (-js doesn't require rule, as it is supported out of the box.)
+ *              {
+ *                  test: creates a rule for 1 or many file extensions. eg: css
+ *                  loader: plugs a loader module to handle that file.
+*               }
+ *
+ *
  * Note: webpack-dev-server doesn't output file. It just servers a temporary file from memory.
  */
 module.exports = {
@@ -15,5 +23,13 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loader: 'css-loader'
+            }
+        ]
     }
 };
